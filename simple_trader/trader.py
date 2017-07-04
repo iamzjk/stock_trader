5afd6f888cb4a84d091cec22e9354c93ef69f31c
Jingkun <iamzjk@hotmail.com>
2017-07-04 17:37:18 -0400

    scrape sp500 list from wikipedia

--- simple_trader/trader.py
+++ simple_trader/trader.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python3.5
 """
-	Trader Functions
+    Trader Functions
 """
 
 import pandas_datareader.data as web
 import datetime
 
 
-def read_data(symbol, start, end=None):
-	"""
-	Args:
-		symbol (str): 'AAPL'
-		start (str): 'YYYY-MM-DD'
-		end (str): 'YYYY-MM-DD'
+def read_data(symbols, start, end=None):
+    """
+    Args:
+        symbols (list or str): 'AAPL', ['AAPL', 'F']
+        start (str): 'YYYY-MM-DD'
+        end (str): 'YYYY-MM-DD'
 
-	Returns:
-		dataframe
-	"""
+    Returns:
+        panel or dataframe
+    """    
 
-	start_date = datetime.datetime.strptime(start, '%Y-%m-%d')
-	today = datetime.datetime.today()
-	end_date = datetime.datetime.strptime(end, '%Y-%m-%d') if end else today
+    start_date = datetime.datetime.strptime(start, '%Y-%m-%d')
+    today = datetime.datetime.today()
+    end_date = datetime.datetime.strptime(end, '%Y-%m-%d') if end else today
 
-	df_data = web.DataReader(symbol, 'google', start_date, end_date)
+    pf = web.DataReader(symbols, 'google', start_date, end_date)
 
-	return df_data
+    return pf
 
 
-def find_events(df_data, lookback=20):
-	"""
-	Args:
-		df_data (dataframe): output of read_data function
-		lookback (int): how many days TAs look back
+def find_events(pf, lookback=20):
+    """
+    Args:
+        pf (panel): output of read_data function
+        lookback (int): how many days TAs look back
 
-	Returns:
-		dataframe
-	"""
+    Returns:
+        dataframe
+    """
 
-	df_data
+    df_close = pd['Close']
+    df_ma = pd.rolling_mean(df_close, lookback)
+    df_std = pd.rolling_std(df_close, lookback)
+    df_bollingerbands = (df_close - df_ma) / df_std
+
 
 if __name__ == '__main__':
 
-	data = read_data('AAPL', '2017-05-01', '2017-06-01')
-	print(data)
+    data = read_data('AAPL', '2017-05-01', '2017-06-01')
+    print(data)
